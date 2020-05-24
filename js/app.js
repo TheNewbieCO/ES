3e0521247f7ad51f1e68079b2b0453bdc7a3e864
gianpier verutti <gianpierverutti@live.com>
2020-05-23 21:58:52 -0500

    Se hicieron cambios sugeridos por Sergiolleite y e

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 !function(e){var t={};
 function n(r){if(t[r])return t[r].exports;
 	var i=t[r]={i:r,l:!1,exports:{}};
-	return e[r].call(i.exports,i,i.export)
+	return e[r].call(i.exports,i,i.export
--- serviceworker.js
+++ serviceworker.js
@@ -1,39 +1,23 @@
-// imports
-// importScripts('js/sw-utils.js');
 
-
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
-const INMUTABLE_CACHE = 'inmutable-v1';
+const STATIC_CACHE = 'static-v1-' + new Date().getTime();
+const DYNAMIC_CACHE = 'dynamic-v1' + new Date().getTime();
+const INMUTABLE_CACHE = 'inmutable-v1' + new Date().getTime();
 
 
 const APP_SHELL = [
-    '/index.php',
-    '/css/style.css',
-    '/images/icono.ico',
-    '/images/empresasolidaria.ico',
-    '/images/apple-touch-icon.png',
-    '/images/AdminLTELogo.png',
-    '/images/icono.png',
-    '/images/hero-img.png',
-    '/images/more-services-1.jpg',
-    '/images/solidaria.png',
-    '/images/logo_v.png',
-    '/js/app.js'
+
 ];
 
+
 const APP_SHELL_INMUTABLE = [
-    '/fonts/font-awesome/css/font-awesome.css'
-    //    'https://use.fontawesome.com/releases/v5.3.1/css/all.css'
-    'js/jquery.js'
+
 ];
 
 
 // Guardar los dos arreglos de cache
-self.addEventListener('install', e => {
+self.addEventListener("install", event => {
 
-
-    const cacheStatic = caches.open(STATIC_CACHE).then(cache =>
+  const cacheStatic = caches.open(STATIC_CACHE).then(cache =>
         cache.addAll(APP_SHELL));
 
     const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache =>
@@ -41,14 +25,12 @@ self.addEventListener('install', e => {
 
 
 
-    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+    event.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 
 });
 
 //  Verificar si hay diferencias entre el cache guardado y el que está en la funete
-
-self.addEventListener('activate', e => {
-
+self.addEventListener('activate', event => {
     const respuesta = caches.keys().then(keys => {
 
         keys.forEach(key => {
@@ -65,7 +47,9 @@ self.addEventListener('activate', e => {
 
     });
 
-    e.waitUntil(respuesta);
+    event.waitUntil(respuesta);
+
+
 
 });
 
@@ -112,8 +96,5 @@ self.addEventListener('fetch', e => {
 
     });
 
-
-
-    e.respondWith(respuesta);
-
+    e.respondWith( respuesta );
 });
